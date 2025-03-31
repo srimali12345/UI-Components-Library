962deea919465d750362a5d6cd06c6a42b89c7d6
subanemisooriyakumaran <110389062+subanemisooriyakumaran@users.noreply.github.com>
2025-03-31 11:27:37 +0530

    ui fix

--- src/components/Button/CodeModal.js
+++ src/components/Button/CodeModal.js
@@ -43,21 +43,21 @@ const CodeModal = ({ buttonType, buttonStyles = {}, buttonText, onClose }) => {
         </div>
 
         <div className="tabs-copy-container">
-          <div className="tabs">
+          <div className="btn-group">
           <button 
-              className={activeTab === "html" ? "active" : ""} 
+              className={activeTab === "html" ? "active btn-outline" : "btn-outline"} 
               onClick={() => setActiveTab("html")}
             >
               HTML
             </button>
             <button 
-              className={activeTab === "css" ? "active" : ""} 
+              className={activeTab === "css" ? "active btn-outline" : "btn-outline"} 
               onClick={() => setActiveTab("css")}
             >
               CSS
             </button>
             <button 
-              className={activeTab === "scss" ? "active" : ""} 
+              className={activeTab === "scss" ? "active btn-outline" : "btn-outline"} 
               onClick={() => setActiveTab("scss")}
             >
               SCSS
@@ -65,7 +65,7 @@ const CodeModal = ({ buttonType, buttonStyles = {}, buttonText, onClose }) => {
            
           </div>
           <button className="copy-btn" onClick={handleCopy}>
-            <Copy size={16} /> Copy Code
+            Copy <Copy size={16} /> 
           </button>
         </div>
         
