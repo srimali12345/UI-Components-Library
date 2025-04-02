8988b8ddcf9128c29f47bacbf3665588f6d5357f
srimali12345 <srimali.ranawaka@1billiontech.com>
2025-04-02 16:33:18 +0530

    navbar customization changes

--- src/components/Navbar/NavbarCustomizer.js
+++ src/components/Navbar/NavbarCustomizer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import PreviewPane from "./PreviewPane";
 import ColorPicker from "./ColorPicker";
 import LogoUploader from "./LogoUploader";
