a8973f3c988aee7e1e4c63a18137ff0af145c93a
srimali <srimali.ranawaka@1billiontech.com>
2025-06-20 13:25:05 +0530

    webpack build changes

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
+    filename: 'index.js',
     publicPath: '/',
   },
   resolve: {
